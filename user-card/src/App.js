f03799367f9de3be56a4449bbce472c2cefdd27c
dmunter2 <devon.munter@gmail.com>
2019-09-08 19:21:08 -0700

    api is working

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios'
+import UserCard from './UserCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      // user: "",
+      followers: [],
+      id: ""
+    }
+  }
+
+
+  componentDidMount(){
+    fetch(`https://api.github.com/users/dmunter2/followers`)
+      .then(res => res.json())
+      .then(users => this.setState({ followers: users }))
+      .catch(err => console.log('hey', err))
+  }
+
+
+
+
+
+
+  render() {
+
+
+    return (
+      <div>
+          <UserCard id={this.state.id} followers={this.state.followers}/>
+        </div>
+    );
+  }
+
 }
 
 export default App;
+
+
+
+/*
+      <div className="App">
+        <input
+          type='text'
+          value={this.state.user}
+          onChange={this.handleChange}
+          />
+        <h1>this is awesome</h1>
+        <div>
+          {this.state.followers.map(followers => {
+            return (
+              <div key={followers}>
+                {this.login}
+              </div>
+            );
+
+          })}
+        </div>
+      </div>
+*/
